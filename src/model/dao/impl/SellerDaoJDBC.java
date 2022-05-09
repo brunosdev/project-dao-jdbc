9c99e14f0c960de8a586102c417af7046cf7de20
Bruno <brunosanttos@hotmail.com.br>
2022-05-09 13:16:40 -0300

    Update DaoFactory and creating DepartmentDaoJDBC

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,13 +1,10 @@
 package model.dao.impl;
 
-import java.io.IOException;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
