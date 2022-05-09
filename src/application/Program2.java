5e837a8f0a78ceb3f839259963a3d4b8f810ed32
Bruno <brunosanttos@hotmail.com.br>
2022-05-09 14:17:28 -0300

    Implementing delete on DepartmentDaoJDBC

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -10,6 +11,8 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
@@ -31,7 +34,15 @@ public class Program2 {
 		Department dep2 = departmentDao.findById(7);
 		dep2.setName("Food");
 		departmentDao.update(dep2);
-		System.out.println("Update completed");
+		System.out.println("Update completed!");
+		
+		System.out.println("\n=== TEST 5: delete =======");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed!");
+		
+		sc.close();
 	}
 
 }
